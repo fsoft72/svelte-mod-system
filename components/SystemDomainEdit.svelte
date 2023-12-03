ab9b323da591a374b5c4e27ebe6c837c38256303
fsoft <fabio.rotondo@gmail.com>
2023-12-03 10:27:50 +0100

    Working SystemDomainsList and SystemDomainEdit components

--- components/SystemDomainEdit.svelte
+++ components/SystemDomainEdit.svelte
@@ -1,7 +1,51 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	import FormCreator from '$liwe3/components/FormCreator.svelte';
+	import type { FormField } from '$liwe3/components/FormCreator.svelte';
 	import type { SystemDomain } from '../types';
 
 	export let domain: SystemDomain;
+
+	let fields: FormField[] = [
+		{
+			name: 'id',
+			label: 'ID',
+			type: 'hidden',
+			required: true
+		},
+		{
+			name: 'code',
+			label: 'Code',
+			type: 'text',
+			required: true,
+			placeholder: 'code',
+			col: 4
+		},
+		{
+			name: 'name',
+			label: 'Name',
+			type: 'text',
+			required: true,
+			placeholder: 'Name',
+			col: 6
+		},
+		{
+			name: 'visible',
+			label: 'Visible',
+			type: 'checkbox',
+			required: false,
+			col: 2
+		}
+	];
+
+	const dispatch = createEventDispatcher();
+
+	const onSubmit = (data: any) => {
+		dispatch('domain', data);
+	};
 </script>
 
-<h1>Edit {domain.code}</h1>
+<div class="container">
+	<FormCreator {fields} values={domain} on:submit={(e) => onSubmit(e.detail)} />
+</div>
--- components/SystemDomainsList.svelte
+++ components/SystemDomainsList.svelte
@@ -2,15 +2,18 @@
 	import Button from '$liwe3/components/Button.svelte';
 	import DataGrid, { type GridAction, type GridDataRow } from '$liwe3/components/DataGrid.svelte';
 	import { _ } from '$liwe3/stores/LocalizationStore';
-	import { has_perm } from '$liwe3/utils/utils';
-	import { user } from '$modules/user/store';
 	import { onMount } from 'svelte';
 	import gridFields from './subs/domains_fields';
 	import type { SystemDomain } from '../types';
 	import Paginator from '$liwe3/components/Paginator.svelte';
-	import { system_admin_domains_list } from '../actions';
+	import {
+		system_admin_domain_add,
+		system_admin_domain_update,
+		system_admin_domains_list
+	} from '../actions';
 	import Modal from '$liwe3/components/Modal.svelte';
 	import SystemDomainEdit from './SystemDomainEdit.svelte';
+	import { PencilSquare, Trash } from 'svelte-hero-icons';
 
 	let currentRow: any = null;
 	let editModalOpen = false;
@@ -22,10 +25,53 @@
 	let displayDomains: GridDataRow[] = [];
 	let filteredDomains: SystemDomain[] = [];
 
-	let actions: GridAction[] = [];
+	let actions: GridAction[] = [
+		{
+			id: 'edit',
+			label: 'Edit',
+			icon: PencilSquare,
+			mode: 'mode1',
+			action: (row: any) => {
+				currentRow = row;
+				editModalOpen = true;
+			}
+		},
+		{
+			id: 'delete',
+			label: 'Delete',
+			icon: Trash,
+			mode: 'error',
+			action: (row: any) => {
+				currentRow = row;
+				deleteModalOpen = true;
+			}
+		}
+	];
 
 	const deleteDomain = async () => {
 		console.log('=== DELETE DOMAIN', currentRow);
+	};
+
+	const onDomain = async (domain: SystemDomain) => {
+		let res;
+
+		if (domain?.id.length) {
+			res = await system_admin_domain_update(domain.id, domain.code, domain.name, domain.visible);
+		} else {
+			res = await system_admin_domain_add(domain.code, domain.name, domain.visible);
+		}
+
+		if (res.error) return;
+
+		updateDomains();
+	};
+
+	const updateDomains = async () => {
+		const res = await system_admin_domains_list();
+
+		if (res.error) return;
+
+		domains = res;
 	};
 
 	$: {
@@ -34,30 +80,23 @@
 	}
 
 	onMount(async () => {
-		const res = await system_admin_domains_list();
-
-		if (res.error) return;
-
-		domains = res;
+		updateDomains();
 	});
 </script>
 
 <div class="container">
 	<div class="buttons">
 		<p class="title">{$_('System Domains List')}</p>
-		{#if has_perm($user, 'user.create')}
-			<Button
-				mode="mode2"
-				size="sm"
-				href="/user/create"
-				on:click={() => {
-					currentRow = { id: '' };
-					editModalOpen = true;
-				}}
-			>
-				{$_('Create user')}
-			</Button>
-		{/if}
+		<Button
+			mode="mode2"
+			size="sm"
+			on:click={() => {
+				currentRow = { id: '' };
+				editModalOpen = true;
+			}}
+		>
+			{$_('Create Domain')}
+		</Button>
 	</div>
 	<DataGrid
 		data={displayDomains}
@@ -84,7 +123,7 @@
 
 {#if deleteModalOpen}
 	<Modal
-		title="Delete user"
+		title={$_('Delete domain')}
 		on:confirm={() => {
 			domains = domains.filter((r) => r.id !== currentRow.id);
 			deleteModalOpen = false;
@@ -93,19 +132,19 @@
 			deleteModalOpen = false;
 		}}
 	>
-		Please confirm you want to delete user<br />
-		<div class="delete-user">{currentRow?.email}</div>
+		{$_('Please confirm you want to delete domain')}<br />
+		<div class="delete-domain">{currentRow?.code} / {currentRow?.name}</div>
 
 		<div slot="footer">
-			<Button mode="error" on:click={deleteDomain}>Delete User</Button>
-			<Button mode="info" on:click={() => (deleteModalOpen = false)}>Cancel</Button>
+			<Button mode="error" on:click={deleteDomain}>{$_('Delete Domain')}</Button>
+			<Button mode="info" on:click={() => (deleteModalOpen = false)}>{$_('Cancel')}</Button>
 		</div>
 	</Modal>
 {/if}
 
 {#if editModalOpen}
 	<Modal
-		title="Edit user"
+		title={$_('Edit domain')}
 		on:confirm={() => {
 			editModalOpen = false;
 		}}
@@ -113,7 +152,7 @@
 			editModalOpen = false;
 		}}
 	>
-		<SystemDomainEdit domain={currentRow} />
+		<SystemDomainEdit domain={currentRow} on:domain={(e) => onDomain(e.detail)} />
 	</Modal>
 {/if}
 
@@ -124,7 +163,7 @@
 		border-radius: var(--liwe-border-radius);
 		padding-bottom: 0.01rem;
 	}
-	.delete-user {
+	.delete-domain {
 		font-weight: bold;
 		text-align: center;
 		padding: 10px;
