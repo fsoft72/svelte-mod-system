aa2f61916d0e4ce23d1d816767ead3cb80d7add4
Fabio Rotondo <fabio.rotondo@gmail.com>
2024-03-22 09:18:29 +0100

    ADD: Export JSON button

--- components/ThemeColorSelector.svelte
+++ components/ThemeColorSelector.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import Button from '$liwe3/components/Button.svelte';
+	import Input from '$liwe3/components/Input.svelte';
+	import { downloadFile } from '$liwe3/utils/utils';
 	import { themeSetMode } from '../theme';
 	import { theme, themeModes, themeSetDarkMode, themeSetModeColor } from '../theme_store';
 
@@ -10,17 +13,29 @@
 		themeSetDarkMode(darkMode);
 		themeSetMode(darkMode ? 'dark' : 'light');
 	};
+
+	const exportJSON = () => {
+		const data = {
+			light: $theme.light,
+			dark: $theme.dark
+		};
+		console.log('=== EXPORT: ', data);
+		const json = JSON.stringify(data, null, 2);
+		downloadFile(json, 'theme.json', 'application/json');
+	};
 </script>
 
 <div class="container">
 	<div class="liwe3-row">
 		<div class="liwe3-col4">
 			<div class="theme-selector">
-				Dark mode &nbsp;&nbsp;<input
+				<Input
 					type="checkbox"
 					checked={$theme.theme === 'dark'}
 					on:change={(e) => setDarkMode(e.target.checked)}
+					label="Dark mode"
 				/>
+				<Button mode="info" on:click={exportJSON}>Export JSON</Button>
 			</div>
 		</div>
 		<div class="liwe3-col8 tweekers">
@@ -53,5 +68,14 @@
 <style>
 	.container {
 		padding: 20px;
+	}
+
+	.theme-selector {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: flex-start;
+
+		height: 100%;
 	}
 </style>
